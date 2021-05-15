bc89c2a5bb7bfae7989aa03de1c762bc49c59e8f
Kwaay <benoit.dum74@gmail.com>
2021-05-15 18:42:37 +0200

    V0.1 - Premier Commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php include('pages/header.php'); ?>
+        <section id="APropos">
+            <div class="container">
+                <div class="text">
+                    <p> 
+                    Aenean mauris nisl, ultricies quis nulla quis, venenatis tempus justo. Maecenas ultrices venenatis porttitor. Nam a eros nunc. Maecenas ac velit ac odio pharetra aliquam et nec sapien. Phasellus vitae sapien sapien. Fusce nec tincidunt dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean non aliquam ipsum. Cras tincidunt lacinia fermentum.
+                    
+                    In semper dapibus justo eget iaculis. Donec id imperdiet diam, vitae dapibus lorem. Ut quis pellentesque lectus. Suspendisse commodo rutrum lobortis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non massa non turpis cursus ultricies eu ut sem. Mauris gravida nunc velit, in tempor tellus tristique et. Maecenas commodo mattis libero. Aliquam lobortis justo leo, at tincidunt diam luctus et. Donec id ipsum vel nisl tempor venenatis ut ut nunc. Proin ac convallis lacus, ac fermentum turpis. Integer erat tortor, eleifend vitae faucibus quis, vestibulum ut lectus. Curabitur fringilla velit eu posuere porttitor. Vivamus pretium, lectus ac sagittis maximus, tortor erat consectetur tortor, vel euismod nunc massa interdum libero.
+                    </p>
+                </div>
+            </div>
+        </section>
+        <br />
+        <section id="Skills">
+            <div class="container">
+                <h3> Mes compétences </h3>
+            </div>
+        </section>
+    </body>
+</html>
+<?php include('pages/footer.php'); ?>
---
+++ pages/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" />
+        <title>Portfolio</title>
+        <link href="style.css" rel="stylesheet" />
+        <meta name="viewport" content="width=device-width, initial-scale=1"> 
+    </head>
+    <body>
+        <section class="header">
+            <div class="logo">
+                 <!-- <img src="images/logo.png" alt="Mon Logo"> -->
+            </div>
+            <div class="buttons">
+                <div class="design-accueil">
+                    <a href="index.php" class="link-button">Accueil</a>
+                </div>
+                <div class="design-a-propos">
+                    <a href="#APropos" class="link-button">A propos de moi</a>
+                </div>
+                <div class="design-reseaux">
+                    <a href="#Skills" class="link-button">Mes compétences</a>
+                </div>
+                <div class="design-contact">
+                    <a href="#Contact" class="link-button">Me contacter</a>
+                </div>
+            </div>
+        </section>
+        
+
+    
